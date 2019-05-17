5423613507f84df8242d705b90659564d291ecfa
Daniel Ko <danko56666@gmail.com>
2019-05-16 22:34:28 -0400

    added filler components

---
+++ client/src/containers/Dummycontainer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Dummycomponent from "../components/Dummycomponent";
+
+export class Dummycontainer extends Component {
+    render() {
+        return <Dummycomponent />;
+    }
+}
+
+export default Dummycontainer;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dummycontainer from "../containers/Dummycontainer";
+
+const Home = () => {
+    return <Dummycontainer />;
+};
+
+export default Home;
