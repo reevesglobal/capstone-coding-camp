ea6eedcf228987736af4abc39aab0e5f413d3413
Cameron Roman <camroman95@gmail.com>
2019-06-20 17:19:40 -0500

    Added background to container instead of component

--- client/src/containers/JumboNavContainer.jsx
+++ client/src/containers/JumboNavContainer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const  ContainerBackground = styled.div`
+    height: 300px;
+    background-image: linear-gradient(#296ba4, #e9f6fb);
+
+`
 
 const JumboNavContainer = (props) => {
     return (
-        <div>
+        <ContainerBackground>
           {props.children}
-        </div>
+        </ContainerBackground>
     );
 }
 
