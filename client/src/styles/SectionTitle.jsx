47b44449eee90150f86f7af851805ab41a088bbd
David N <bigair_1080@hotmail.com>
2019-06-20 14:00:05 -0300

    Font-weight
    Changed the font-weight back to how it was. I think it looks better that way ;)

--- client/src/styles/SectionTitle.jsx
+++ client/src/styles/SectionTitle.jsx
@@ -5,7 +5,7 @@ const StyledTitle = styled.h2`
   padding-top: ${props => props.inputPadding || "10px"};
   font-family: 'Roboto', sans-serif;
   font-size: 3.25em;
-  font-weight: 400;
+  font-weight: 300;
   text-align: center;
   color: ${props => props.inputColor || "#A50000"};
   text-transform: uppercase;
