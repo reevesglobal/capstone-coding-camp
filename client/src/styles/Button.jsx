83f6089d90e65b99a324d1587e714346fc8cb37b
David N <bigair_1080@hotmail.com>
2019-07-02 16:37:05 -0300

    Syntax fixes

--- client/src/styles/Button.jsx
+++ client/src/styles/Button.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-    background: ${props => props.background } none;
+    background: ${props => props.background || "none"};
     color: ${props => props.color || "white"};
     :hover {
         color: ${props => props.hoverColor};
-        background: ${props => props.background} white;
+        background: ${props => props.background || "white"};
     }
     font-size: 20px;
     font-family: verdana;
     margin: 0px 80px 20px 50px;
     padding: 8px 16px;
-    border: ${props => props.border } none;
+    border: ${props => props.border || "none"};
     border-radius: 10px;
     cursor: pointer;
-
-
-`
-
-
+`;
 
 const Button = (props) => {
     return (
@@ -30,7 +26,7 @@ const Button = (props) => {
             border={props.border}
             hoverColor={props.hoverColor}
         >
-        {props.text}
+            {props.text}
         </StyledButton>
     );
 }
